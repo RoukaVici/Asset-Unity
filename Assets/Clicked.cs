69c83b5a7ac5d0a1bc908156203cb0c8e4631666
DasFranck <dasfranck@gmx.com>
2017-03-22 20:01:50 +0100

    Adding the first draft.

---
+++ Assets/Clicked.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicked : MonoBehaviour {
+	public char motorNb;
+	public string assignedKey;
+	private bool vibrating = false;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (assignedKey))
+		{
+			vibrating = true;
+			GetComponent<Renderer> ().material.color = Color.green;
+			print ("Activating Motor " + motorNb);
+			SendingToArduino.sendToArduino (motorNb, vibrating);
+		} 
+		else if (Input.GetKeyUp (assignedKey))
+		{
+			vibrating = false;
+			GetComponent<Renderer> ().material.color = Color.red;
+			print("Deactivating Motor " + motorNb);
+			SendingToArduino.sendToArduino (motorNb, vibrating);
+		}
+	}
+
+	void OnMouseOver()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			vibrating = !vibrating;
+			GetComponent<Renderer> ().material.color = (vibrating ? Color.green : Color.red);
+			print ((vibrating ? "Activating Motor " : "Deactivating Motor ") + motorNb);
+			SendingToArduino.sendToArduino (motorNb, vibrating);
+		}
+	}
+}
---
+++ Assets/SendingToArduino.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System.Threading;
+
+public class SendingToArduino : MonoBehaviour {
+	public static SerialPort sp = new SerialPort("COM4", 9600);
+
+	// Use this for initialization
+	void Start () {
+		OpenConnection ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenConnection()
+	{
+		if (sp != null) {
+			if (sp.IsOpen) {
+				sp.Close ();
+				print ("Closing port, because it was already in use.");
+			} else {
+				sp.Open ();
+				sp.ReadTimeout = 16;
+				print ("Port succesfully opened.");
+			}
+		}
+		else if (sp.IsOpen)
+			print ("Port is already in use.");
+		else
+			print ("Port is null");
+	}
+
+	void OnApplicationQuit()
+	{
+		sp.Close ();
+	}
+
+	public static void sendToArduino(char motorNb, bool vibrate)
+	{
+		sp.Write (new[] {motorNb, (vibrate ? (char)1 : (char)0)}, 0, 2);
+	}
+}
