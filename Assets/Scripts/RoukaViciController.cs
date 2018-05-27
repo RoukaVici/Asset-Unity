aae1706f2d54f681906de48d6d3186b93e6f7bc2
Louis Jahn <louis.jahn@epitech.eu>
2018-05-26 17:42:16 -0700

    Added .bundle files for macOS libraries

---
+++ Assets/Scripts/RoukaViciController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoukaViciController : MonoBehaviour {
+	[SerializeField]
+	private Color selectedItemColor;
+	private Color currentItemColor;
+	private static RoukaViciController _instance;
+	public static RoukaViciController instance {
+		get {
+			if (_instance == null) {
+				_instance = GameObject.FindObjectOfType<RoukaViciController>();
+			}
+			return _instance;
+		}
+	}
+
+	public List<VibrationStyle> vibrationPatterns = new List<VibrationStyle>();
+	public List<GameObject> patternButtons = new List<GameObject>();
+	public int patternID = 0;
+
+	void Awake() {
+		if (_instance == null)
+			_instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void setVibrationPattern(int newID) {
+		newID = newID < 0 ? 0 : newID;
+		if (newID == patternID)
+			return ;
+		patternButtons[patternID].GetComponent<PatternData>().background.color = currentItemColor;
+		patternID = newID;
+		patternButtons[patternID].GetComponent<PatternData>().background.color = selectedItemColor;
+	}
+
+	// Use this for initialization
+	void Start () {
+		PatternData data = patternButtons[patternID].GetComponent<PatternData>();
+		currentItemColor = data.background.color;
+		data.background.color = selectedItemColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SliderInputfieldLink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderInputfieldLink : MonoBehaviour {
+	[SerializeField]
+	InputField inputFieldIntensity;
+
+	[SerializeField]
+	Slider sliderIntensity;
+
+	void Start() {
+		if (inputFieldIntensity && sliderIntensity) {
+			inputFieldIntensity.onValueChanged.AddListener(delegate {InputFieldUpdate();});
+			sliderIntensity.onValueChanged.AddListener(delegate {SliderUpdate();});
+		}
+	}
+
+	private void SliderUpdate() {
+		inputFieldIntensity.text = sliderIntensity.value.ToString();
+	}
+
+	private void InputFieldUpdate() {
+		int value = 0;
+		if (inputFieldIntensity.text.Length > 0 && inputFieldIntensity.text.ToCharArray()[0] == '-')
+			inputFieldIntensity.text = "0";
+		else if (inputFieldIntensity.text.Length > 0)
+			value = int.Parse(inputFieldIntensity.text);
+		sliderIntensity.value = Mathf.Clamp(value, 0, 100);
+	}
+}
