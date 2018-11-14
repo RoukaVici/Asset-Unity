504d98666848d8b8ac3211f66ad953e71466d178
Unknown <louis.jahn@epitech.eu>
2018-11-14 22:35:58 +0100

    Better interactions with UI
    Show and hide the UI by the press of a configurable button, connect manually with a button, see the status of the connection

---
+++ Assets/RoukaVici/Scripts/StatusData.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusData : MonoBehaviour
+{
+	[SerializeField] GameObject connectedImage, errorImage, loadingImage;
+	Animator connectedImageAnimator;
+	void Start ()
+	{
+		connectedImage.SetActive(false);
+		errorImage.SetActive(true);
+		loadingImage.SetActive(false);
+
+		connectedImageAnimator = loadingImage.GetComponent<Animator>();
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	private IEnumerator ConnectionCoroutine()
+	{
+		if (LibRoukaVici.TryBluetoothConnection() == 0)
+		{
+			connectedImage.SetActive(true);
+			Debug.Log("RoukaVici is connected");
+		}
+		else
+		{
+			errorImage.SetActive(true);
+			Debug.Log("Something went wrong during connection");
+		}
+
+		connectedImageAnimator.StopPlayback();
+		loadingImage.SetActive(false);
+		yield return null;
+	}
+
+	public void Connect()
+	{
+		errorImage.SetActive(false);
+		loadingImage.SetActive(true);
+
+		connectedImageAnimator.Play("Rotation");
+		StartCoroutine(ConnectionCoroutine());
+	}
+}
