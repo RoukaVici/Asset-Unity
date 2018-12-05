af4d31a6e60dcf31ebd19db7a9d5ece52984966a
Unknown <louis.jahn@epitech.eu>
2018-12-05 02:09:14 +0100

    Slot Animations

---
+++ Assets/RoukaVici/Scripts/FadeOnOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FadeOnOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	[SerializeField] Animation[] contentToFade;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+		foreach (Animation anim in contentToFade)
+		{
+			if (anim.gameObject.GetComponent<Image>().color.a == 0)
+				anim.Play("FadeIn");
+		}
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+	{
+		foreach (Animation anim in contentToFade)
+			anim.Play("FadeOut");
+	}
+}
---
+++ Assets/RoukaVici/Scripts/ReplaceImageOnHover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ReplaceImageOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	[SerializeField] Sprite texture;
+	Image image;
+	Sprite previousTexture;
+	Animation anim;
+
+	void Start()
+	{
+		image = GetComponent<Image>();
+		anim = GetComponent<Animation>();
+		previousTexture = image.sprite;
+	}
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+		if (anim)
+			anim.Stop();
+		image.color = new Color(image.color.r, image.color.g, image.color.b, 1);
+		image.sprite = texture;
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+	{
+		image.sprite = previousTexture;
+	}
+}
